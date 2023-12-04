1e5e3ba5fbbfaf4e45b6509e3ef1bc6fcfe48cc3
groupCMPR <144290496+groupCMPR@users.noreply.github.com>
2023-12-04 11:47:59 -0800

    Update SetupMain.cpp

--- SetupMain.cpp
+++ SetupMain.cpp
@@ -3,9 +3,13 @@
 //Description: Chapter 13 Assignment - Sortings
 
 #include <iostream> //For cout
+#include <vector>   //For vector
+#include <iomanip>  //For formatting output
 
 //HEADER FILES
-#include "input.h" //For input validation
+#include "input.h"           //For input validation
+#include "SortSimulations.h" //For option 1
+#include "StudentRecord.h"   //For option 2
 using namespace std;
 
 //PROTOTYPES
@@ -54,6 +58,8 @@ int menuOption()
 //Postcondition: Simulate the different sorts
 void option1()
 {
+    SortSimulations<double> sort_array;
+
     do
     {
         system("cls");
@@ -74,19 +80,140 @@ void option1()
         switch (inputChar("\n\t\tOption: ", static_cast<string>("ABCDEFGH0")))
         {
         case '0': return;
-        case 'A': break;
-        case 'B': break;
-        case 'C': break;
-        case 'D': break;
-        case 'E': break;
-        case 'F': break;
-        case 'G': break;
-        case 'H': break;
+        case 'A':
+        {
+            int size = inputInteger("\n\t\tEnter the size of the dynamic array: ", true);
+            sort_array.set_Array(size);
+            cout << "\n\t\tDynamic array's size of " << sort_array.get_Size() << " has been created with random elements.\n";
+        }break;
+        case 'B':
+        {
+            if (sort_array.is_Empty()) {
+                cout << "\n\t\tArray is empty.\n";
+                break;
+            }
+
+            sort_array.displayUnsorted();
+        }break;
+        case 'C': 
+        {
+            sort_array.set_sort_array();
+
+            if (sort_array.is_Empty()) {
+                cout << "\n\t\tArray is empty.\n";
+                break;
+            }
+
+            char select = inputChar("\n\t\tBubbleSort: \n\t\tChoose sort in (A)scending or (D)escending order: ", static_cast<string>("AD"));
+
+            if (select == 'A')
+                sort_array.bubble_sort_recurse(0, &SortSimulations<double>::lesser_compare);
+            else
+                sort_array.bubble_sort_recurse(0, &SortSimulations<double>::greater_compare);
+
+            cout << sort_array;
+            sort_array.reset_Count();
+        }break;
+        case 'D': 
+        {
+            sort_array.set_sort_array();
+
+            if (sort_array.is_Empty()) {
+                cout << "\n\t\tArray is empty.\n";
+                break;
+            }
+
+            char select = inputChar("\n\t\tSelectionSort: \n\t\tChoose sort in (A)scending or (D)escending order: ", static_cast<string>("AD"));
+
+            if (select == 'A')
+                sort_array.selection_sort_recurse(0, &SortSimulations<double>::greater_compare);
+            else
+                sort_array.selection_sort_recurse(0, &SortSimulations<double>::lesser_compare);
+
+            cout << sort_array;
+            sort_array.reset_Count();
+        } break;
+        case 'E': 
+        {
+            sort_array.set_sort_array();
+
+            if (sort_array.is_Empty()) {
+                cout << "\n\t\tArray is empty.\n";
+                break;
+            }
+
+            char select = inputChar("\n\t\tInsertionSort: \n\t\tChoose sort in (A)scending or (D)escending order: ", static_cast<string>("AD"));
+
+            if (select == 'A')
+                sort_array.insertion_sort_recurse(1, &SortSimulations<double>::greater_compare);
+            else
+                sort_array.insertion_sort_recurse(1, &SortSimulations<double>::lesser_compare);
+
+            cout << sort_array;
+            sort_array.reset_Count();
+        }break;
+        case 'F': 
+        {
+            sort_array.set_sort_array();
+
+            if (sort_array.is_Empty()) {
+                cout << "\n\t\tArray is empty.\n";
+                break;
+            }
+
+            char select = inputChar("\n\t\tQuickSort: \n\t\tChoose sort in (A)scending or (D)escending order: ", static_cast<string>("AD"));
+
+            if (select == 'A')
+                sort_array.quick_sort(0, sort_array.get_Size() - 1, &SortSimulations<double>::greater_compare);
+            else
+                sort_array.quick_sort(0, sort_array.get_Size() - 1, &SortSimulations<double>::lesser_compare);
+
+            cout << sort_array;
+            sort_array.reset_Count();
+        }break;
+        case 'G': 
+        {
+            sort_array.set_sort_array();
+
+            if (sort_array.is_Empty()) {
+                cout << "\n\t\tArray is empty.\n";
+                break;
+            }
+
+            char select = inputChar("\n\t\tMergeSort: \n\t\tChoose sort in (A)scending or (D)escending order: ", static_cast<string>("AD"));
+
+            if (select == 'A')
+                sort_array.mergesort(0, sort_array.get_Size() - 1, &SortSimulations<double>::lesser_compare);
+            else
+                sort_array.mergesort(0, sort_array.get_Size() - 1, &SortSimulations<double>::greater_compare);
+
+            cout << sort_array;
+            sort_array.reset_Count();
+        } break;
+        case 'H': 
+        {
+            sort_array.set_sort_array();
+
+            if (sort_array.is_Empty()) {
+                cout << "\n\t\tArray is empty.\n";
+                break;
+            }
+
+            char select = inputChar("\n\t\tHeapSort: \n\tChoose sort in (A)scending or (D)escending order: ", static_cast<string>("AD"));
+
+            if (select == 'A')
+                sort_array.sort_heap_recurse(sort_array.get_Size() - 1, &SortSimulations<double>::lesser_compare);
+            else
+                sort_array.sort_heap_recurse(sort_array.get_Size() - 1, &SortSimulations<double>::greater_compare);
+
+            cout << sort_array;
+            sort_array.reset_Count();
+        } break;
         default: cout << "\t\tERROR: - Invalid option. Please re-enter."; break;
         }
         cout << "\n";
         system("pause");
-        
+
     } while (true);
 }
 
@@ -94,6 +221,8 @@ void option1()
 //Postcondition: Apply a sort of student records
 void option2()
 {
+    StudentRecord students;
+
     do
     {
         system("cls");
@@ -112,12 +241,44 @@ void option2()
         switch (inputChar("\n\t\tOption: ", static_cast<string>("ABCDEF0")))
         {
         case '0': return;
-        case 'A': break;
-        case 'B': break;
-        case 'C': break;
-        case 'D': break;
-        case 'E': break;
-        case 'F': break;
+        case 'A':
+            students.readData("Students.dat");
+
+            break;
+        case 'B':
+            cout << "\n\tStudent records:\n";
+            students.displayRecords();
+            break;
+        case 'C': {
+            Student newStudent;
+
+            newStudent.id = inputInteger("\n\tEnter a new student ID: ", true);
+            newStudent.name = inputString("\n\tEnter the student's name: ", true);
+            newStudent.major = inputString("\n\tEnter the student's major: ", true);
+            newStudent.gpa = inputDouble("\n\tEnter the student's GPA: ", 0.0, 4.0);
+
+            students.insertRecord(newStudent);
+            cout << "\n\tNew student has been inserted.";
+            break;
+        }
+        case 'D':
+            students.remove(inputString("\n\tEnter a student's name to remove: ", true));
+            break;
+        case 'E': {
+            StudentRecord sortVersion = students;
+            char sortOrder = inputChar("\n\tChoose sort in (A)-ascending or (D)-descending order: ", 'A', 'D');
+            char sortBy = inputChar("\n\tChoose by (I)-ID, (N)-name, (M)-major or (G)-GPA: ", static_cast<string>("INMG"));
+
+            sortOrder == 'A' ? cout << "\n\tAscending order:\n" : cout << "\n\tDescending order:\n";
+
+            sortVersion.sortRecords(sortOrder, sortBy);
+            sortVersion.displayRecords();
+            break;
+        }
+        case 'F':
+            students.writeData("students.dat");
+            cout << "\n\tData have been saved to students.dat.";
+            break;
         default: cout << "\t\tERROR: - Invalid option. Please re-enter."; break;
         }
         cout << "\n";
